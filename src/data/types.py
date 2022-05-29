b51309578240d56d98c344e9d427b6b7fa6f2a15
m-jeu <maartenjeu@gmail.com>
2022-05-29 13:37:39 +0200

    Change snd element in IMC type to Series

--- src/data/types.py
+++ src/data/types.py
@@ -5,4 +5,4 @@ import pandas as pd
 
 # Because the files we're loading come combined as both a portion that contains information about the measurements
 # and the actual measurements, we'll combine both in one datatype so we can easily pass them together.
-InformationMeasurementCombo = Tuple[pd.DataFrame, pd.DataFrame]
+InformationMeasurementCombo = Tuple[pd.DataFrame, pd.Series]
